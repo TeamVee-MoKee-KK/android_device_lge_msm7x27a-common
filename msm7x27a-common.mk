cf989f435b236dd94481567cc26cd825f677b5cf
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-12 00:27:29 -0300

    msm7x27a: Moved to vendor
    * maybe i remove this ifneq variable after, because no one is special
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -48,7 +48,6 @@ PRODUCT_COPY_FILES += \
 # Wlan
 ifneq (exists, $(shell test -d $(TOP)/vendor/qcom/proprietary/wlan/volans && echo exists))
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wlan/wlan.ko:system/lib/modules/wlan.ko \
     $(LOCAL_PATH)/wlan/firmware/WCN1314_qcom_wlan_nv.bin:system/etc/firmware/wlan/volans/WCN1314_qcom_wlan_nv.bin \
     $(LOCAL_PATH)/wlan/firmware/WCN1314_qcom_fw.bin:system/etc/firmware/wlan/volans/WCN1314_qcom_fw.bin \
     $(LOCAL_PATH)/wlan/firmware/WCN1314_cfg.dat:system/etc/firmware/wlan/volans/WCN1314_cfg.dat \
