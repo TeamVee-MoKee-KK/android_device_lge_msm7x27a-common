a403a264107241d1459b263566a4c70465284751
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-13 23:56:40 -0300

    msm7x27a-common: Copyright-ify
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- nfc.mk
+++ nfc.mk
@@ -1,12 +1,11 @@
-# Copyright (C) 2014 The CyanogenMod Project
-# Copyright (c) 2015 The TeamHackLG Project
-# Copyright (c) 2016 The TeamVee-M4 Project
+# Copyright 2016 The Android Open Source Project
+# Copyright 2016 TeamHacklG
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
