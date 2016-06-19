d24161dd18bc0c392474bd3f86dfa10d6b992528
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-06-18 23:48:31 -0300

    msm7x27a-common: Update to Lollipop/Google Media Codecs
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -72,6 +72,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+# Google Media Codecs
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
+
 # Display HAL
 PRODUCT_PACKAGES += \
     gralloc.msm7x27a \
