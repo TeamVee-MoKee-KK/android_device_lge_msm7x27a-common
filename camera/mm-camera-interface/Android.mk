e0a01ef250798dc2d629aa4621ec7c76da474840
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-07-02 20:45:06 -0300

    camera: Update camera interface makefile to LP
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- camera/mm-camera-interface/Android.mk
+++ camera/mm-camera-interface/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH:= $(call my-dir)
 LOCAL_DIR_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-MM_CAM_FILES:= \
+MM_CAM_FILES := \
         mm_camera_interface2.c \
         mm_camera_stream.c \
         mm_camera_channel.c \
@@ -16,21 +16,17 @@ MM_CAM_FILES:= \
         mm_camera_helper.c \
         mm_omx_jpeg_encoder.c
 
-LOCAL_CFLAGS+= -D_ANDROID_
+LOCAL_CFLAGS += -D_ANDROID_
 LOCAL_COPY_HEADERS_TO := mm-camera-interface
 LOCAL_COPY_HEADERS += mm_camera_interface2.h
 LOCAL_COPY_HEADERS += mm_omx_jpeg_encoder.h
 
-LOCAL_C_INCLUDES+= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-LOCAL_C_INCLUDES+= $(LOCAL_PATH)/..
-LOCAL_C_INCLUDES+= $(LOCAL_PATH)/../inc
-LOCAL_C_INCLUDES+= hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)/mm-core/inc
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../inc
+LOCAL_C_INCLUDES += $(call project-path-for,qcom-media)/mm-core/inc
 
-LOCAL_CFLAGS += -include bionic/libc/kernel/arch-arm/asm/posix_types.h
-LOCAL_CFLAGS += -include bionic/libc/kernel/arch-arm/asm/byteorder.h
-LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/posix_types.h
-LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/types.h
 LOCAL_CFLAGS += -include bionic/libc/include/sys/socket.h
 LOCAL_CFLAGS += -include bionic/libc/include/netinet/in.h
 LOCAL_CFLAGS += -include bionic/libc/include/sys/un.h
