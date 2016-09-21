4b90c93b7a4e85228cd71824a3627659ccc8a4ae
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-09-21 08:15:53 -0300

    msm7x27a: Disable dex-pre-optimization
    * without it, we have more space on /system, but cost more space
      on /data, good for first generation, bad for second generation
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,10 +126,6 @@ USE_MINIKIN := true
 # Use dlmalloc instead of jemalloc for mallocs on low-ram targets
 MALLOC_IMPL := dlmalloc
 
-# Enable dex-preoptimization to speed up first boot sequence
-WITH_DEXPREOPT := true
-DONT_DEXPREOPT_PREBUILTS := true
-
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
