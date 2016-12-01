ab5faa89ffbce9d86a60ce1540e4962b15e41f55
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-12-01 11:37:56 -0200

    Revert "msm7x27a: Add init.d script to fix OpenGAPPS"
    This reverts commit 5f2b8e4aa41ab9a1300d5378bbc39cdc667da78b.

--- msm7x27a-common.mk
+++ msm7x27a-common.mk
@@ -82,10 +82,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
-# GAPPS
-PRODUCT_COPY_FILES += \
-    device/lge/msm7x27a-common/rootdir/system/etc/init.d/99googlelatinimefix:system/etc/init.d/99googlelatinimefix
-
 # Display HAL
 PRODUCT_PACKAGES += \
     gralloc.msm7x27a \
